3c8547752063694f148dc0df95cb7be6fd4c76a7
LvNamm <keocosy@gmail.com>
2022-11-17 23:50:23 +0700

    17-11-2022-3

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,178 +1,200 @@
 @extends('layout')
-@section('title-page','Mây che đan mỹ nghệ | Kingbamnboo')
+@section('title-page', 'Mây che đan mỹ nghệ | Kingbamnboo')
 @section('main-content')
-<style>
-    .w-20 {
-        width: 20% !important;
-    }
-</style>
-<div class="container">
-    <div class="row">
-        <div class="col-md-6">
+    <style>
+        .w-20 {
+            width: 20% !important;
+        }
+    </style>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
 
-            <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel" data-bs-interval="false">
-                <div class="carousel-inner" style="border:1px solid rgb(122, 240, 122)">
-                    <div class="carousel-item active">
-                        <img src="/images/products/lotly/bia.jpg" class="d-block w-100" alt="...">
-                    </div>
+                <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel"
+                    data-bs-interval="false">
+                    <div class="carousel-inner" style="border:1px solid rgb(122, 240, 122)">
+                        <div class="carousel-item active">
+                            <img src="/images/products/lotly/bia.jpg" class="d-block w-100" alt="...">
+                        </div>
 
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/6.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/7.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/8.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/9.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/10.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/11.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/12.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/products/lotly/5.jpg" class="d-block w-100" alt="...">
-                    </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/6.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/7.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/8.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/9.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/10.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/11.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/12.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/products/lotly/5.jpg" class="d-block w-100" alt="...">
+                        </div>
 
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="prev">
+
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="next">
+
+                    </button>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    
-                </button>
+
+                <!-- style="white-space:nowrap;overflow-x: scroll;" -->
+                <div class="mt-2" style="display:flex; flex-wrap: wrap;">
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/bia.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/6.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/7.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/8.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/9.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/10.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/11.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/12.jpg"
+                            alt="">
+                    </div>
+                    <div class="pe-1 pb-1 w-20">
+                        <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/15.jpg"
+                            alt="">
+                    </div>
+                </div>
+
             </div>
 
-            <!-- style="white-space:nowrap;overflow-x: scroll;" -->
-            <div class="mt-2" style="display:flex; flex-wrap: wrap;">
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/bia.jpg" alt="">
+            <div class=" ps-5 col-md-6">
+                <div class="mt-3 mb-3">
+                    <b>
+                        <h5>Lót ly mây tre đan - King Bamboo</h5>
+                    </b>
                 </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/6.jpg" alt="">
-                </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/7.jpg" alt="">
-                </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100"  src="/images/products/lotly/8.jpg" alt="">
-                </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/9.jpg" alt="">
-                </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/10.jpg" alt="">
-                </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/11.jpg" alt="">
-                </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/12.jpg" alt="">
-                </div>
-                <div class="pe-1 pb-1 w-20">
-                    <img style="border:1px solid rgba(0,0,0,0.3)" class="w-100" src="/images/products/lotly/15.jpg" alt="">
-                </div>
-            </div>
-
-        </div>
-
-        <div class=" ps-5 col-md-6">
-            <div class="mt-3 mb-3">
-                <b>
-                    <h5>Lót ly mây tre đan - King Bamboo</h5>
-                </b>
-            </div>
 
 
-            <div>
-                <p>20,000đ</p>
-            </div>
+                <div>
+                    <p>20,000đ</p>
+                </div>
 
-            <div>
-                <p>Phân loại</p>
-            </div>
+                <div>
+                    <p>Phân loại</p>
+                </div>
 
-            <div style="display:flex;">
-                <p style="margin-right: 150px;">Số lượng</p>
-                <div class="center" style="
+                <div style="display:flex;">
+                    <p style="margin-right: 150px;">Số lượng</p>
+
+                    <a target="_blank" href="https://m.me/kingbamboo.vn" class="center me-1"
+                        style="justify-content: flex-start; padding: 0 10px 0 10px;background-color:#23dd23;border:1px solid rgb(35, 231, 61, 0.0);border-radius:20px">
+                        <img style="width:30px" src="/images/contact/shopee.png" alt="">
+                        <p class="ms-2 me-2 mt-2 mb-2" style="color: white;">Shopee</p>
+                    </a>
+
+                    <div class="center"
+                        style="
                                 background-color: #1ccb1c;
                                 border: 1px solid white;
                                 border-radius: 30px;">
-                    <p style="color: white" class="ps-3 pe-3 pb-0 mt-1 mb-1">Đặt hàng ngay</p>
+                        <p style="color: white" class="ps-3 pe-3 pb-0 mt-1 mb-1">Đặt hàng ngay</p>
+                    </div>
                 </div>
-            </div>
 
-            <br>
-            <br>
-            <hr>
-            <br>
-            <br>
-            <div>
-                <p>
-                    <b>SKU</b>
-                    &nbsp Nam
-                </p>
-            </div>
+                <br>
+                <br>
+                <hr>
+                <br>
+                <br>
+                <div>
+                    <p>
+                        <b>SKU</b>
+                        &nbsp Nam
+                    </p>
+                </div>
 
-            <div>
-                <p>
-                    <b>Chất liệu:</b>
-                    &nbsp Nam
-                </p>
-            </div>
+                <div>
+                    <p>
+                        <b>Chất liệu:</b>
+                        &nbsp Nam
+                    </p>
+                </div>
 
-            <div>
-                <p>
-                    <b>Xuất khẩu:</b>
-                    &nbsp Nam
-                </p>
-            </div>
+                <div>
+                    <p>
+                        <b>Xuất khẩu:</b>
+                        &nbsp Nam
+                    </p>
+                </div>
 
-            <div>
-                <p>
-                    <b>Bao bì đóng gói:</b>
-                    &nbsp Nam
-                </p>
-            </div>
+                <div>
+                    <p>
+                        <b>Bao bì đóng gói:</b>
+                        &nbsp Nam
+                    </p>
+                </div>
 
-            <div>
-                <p>
-                    <b>Công dụng:</b>
-                    &nbsp Nam
-                </p>
-            </div>
+                <div>
+                    <p>
+                        <b>Công dụng:</b>
+                        &nbsp Nam
+                    </p>
+                </div>
 
-            <div>
-                <p>
-                    <b>Mô tả sản phẩm:</b>
-                    &nbsp Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rem nemo sed qui tenetur delectus laboriosam ipsam enim. Quae error doloremque nesciunt necessitatibus quod distinctio tenetur accusamus asperiores a eum.
-                </p>
-            </div>
+                <div>
+                    <p>
+                        <b>Mô tả sản phẩm:</b>
+                        &nbsp Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rem nemo sed qui tenetur
+                        delectus laboriosam ipsam enim. Quae error doloremque nesciunt necessitatibus quod distinctio
+                        tenetur accusamus asperiores a eum.
+                    </p>
+                </div>
 
-            <div>
-                <p>
-                    <b>Lưu ý khi sử dụng:</b>
-                    &nbsp Nam
-                </p>
-            </div>
+                <div>
+                    <p>
+                        <b>Lưu ý khi sử dụng:</b>
+                        &nbsp Nam
+                    </p>
+                </div>
 
+
+            </div>
 
         </div>
-
     </div>
-</div>
-</div>
+    </div>
 
 
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
 
 @endsection
